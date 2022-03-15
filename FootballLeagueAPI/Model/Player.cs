5184bc0acfe9440825248f6ca246ca054c690123
marko vujic <marko.vujic@comparis.ch>
2022-03-15 20:40:58 +0100

    Initial commit

---
+++ FootballLeagueAPI/Model/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballLeagueAPI.Model
+{
+
+    public enum Position
+    {
+        Goalkeeper = 1,
+        Defender = 2,
+        Midfielder = 3,
+        Attacker = 4
+    }
+    public class Player
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public Position Position { get; set; }
+
+        public int Age { get; set; }
+
+        public string Nationality { get; set; }
+    }
+}
---
+++ FootballLeagueAPI/Model/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballLeagueAPI.Model
+{
+    public class Team
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public List<Player> Players { get; set; }
+    }
+}
